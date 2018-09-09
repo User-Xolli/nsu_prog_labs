8bcae6cfdf277606f33fcced4c806639a6a05d7f
Xolli <zzzzzzzzz1122@gmail.com>
2018-09-09 19:35:50 +0700

    New version lab Kazak Igor

--- lab0/src/main.c
+++ lab0/src/main.c
@@ -2,116 +2,135 @@
 #include<ctype.h>
 #include<math.h>
 #include<string.h>
+#define MAX_LEN_NUMBER 15
 
 double pow_int (int a, int b)
 {
     double ans = 1;
-    if (b == 0)
-        return 1;
 
-    for (int i = 0; i < b; ++i)
-        ans *= (double)a;
+    if (b == 0) return 1;
+    if (a == 0) return 0;
 
-    for (int i = 0; i > b; --i)
-        ans /= (double)a;
+    for (int i = 0; i < b; ++i) ans *= (double)a;
+
+    for (int i = 0; i > b; --i) ans /= (double)a;
 
     return ans;
 }
 
-int main() {
-    int b1, b2;
-    scanf("%d %d\n", &b1, &b2);
-    char X[13];
-    scanf("%s", X);
+int power_number (char* X, int len)
+{
+    int ans = 0;
+    int is_integer_part = 1;
 
-    int len = 0;
-    int power = 0;
-    int flag_point = 1;
-
-    while (X[len] != '\0') {
-        if (X[len] == '.' && flag_point)
-        {
-            flag_point = 0;
-            if (X[len + 1] == '\0' || len == 0)
-            {
-                printf("bad input\n");
-                return 0;
-            }
-        }
-        else if (X[len] == '.')
-        {
-            printf("bad input\n");
-            return 0;
-        }
-        power += flag_point;
-        ++len;
-    }
-    --power;
-
-    double number = 0;
-
-    for (int i = 0; i < len; ++i) {
-        if (X[i] >= 'a' && X[i] <= 'z') {
-            X[i] = toupper(X[i]);
-            if (b1 <= 10 || X[i] > ('A' + b1 - 11)) {
-                printf("bad input\n");
-                return 0;
-            }
-        }
-        if ((b1 <= 10 && isalpha(X[i])) || (isalpha(X[i]) && X[i] > ('A' + b1 - 11)) || (b1 <= 10 && X[i] - '0' >= b1) || (b1 < 2) || (b2 < 2) || (b1 > 16) || (b2 > 16))
-        {
-            printf("bad input\n");
-            return 0;
-        }
-
-        if (isalpha(X[i])) {
-            number += (double)((X[i] - 'A' + 10) * pow_int(b1, power));
-        } else if (X[i] != '.') {
-            number += (double)((double)(X[i] - '0') * pow_int(b1, power));
-        }
-        power -= X[i] != '.';
-    }
-
-    double fraction = modf(number, &number);
-    long long whole = (long long)number;
-    char whole_str[50];
-    int i = 0;
-
-    if (whole == 0)
-        printf("0");
-
-    while (whole != 0)
+    for (int i = 0; i < len; ++i)
     {
-        int digit = whole % b2;
-        if (digit <= 9)
-            whole_str[i] = '0' + digit;
-        else
-            whole_str[i] = 'A' + digit - 10;
+        if (X[i] == '.' && is_integer_part)
+        {
+            is_integer_part = 0;
+            if (X[i + 1] == '\0' || i == 0) return -1;
+        }
+        else if (X[i] == '.') return -1;
+
+        ans += is_integer_part;
+    }
+    return ans - 1;
+}
+
+int bad_digit(char x, int base)
+{
+    return (base <= 10 && isalpha(x)) || //Если осн. СС <= 10 и в записи числа есть буква то bad input
+        (isalpha(x) && x > ('A' + base - 11)) || //(base - 11) - сдвиг максимально возможной буквы в СС с осн. base относительно 'A'
+        (base <= 10 && x - '0' >= base); // (x - '0') перевод цифры x из строки в число
+}
+
+void print_whole(long long x, int base)
+{
+    int i = 0;
+    char x_str[50]; // макс длина числа меньше 50 т.к. при переводе числа с 12 цифрами и точкой из СС с осн. 16
+    // в СС с осн. 2 получим число длиной 12 * 4 + 1 = 49
+
+    if (x == 0) printf("0");
+
+    while (x != 0)
+    {
+        int digit = x % base;
+
+        if (digit <= 9) x_str[i] = '0' + digit;
+
+        else x_str[i] = 'A' + digit - 10;
+
         ++i;
-        whole /= b2;
+        x /= base;
     }
     --i;
 
-    while (i >= 0)
-    {
-        printf("%c", whole_str[i]);
-        --i;
-    }
+    for (; i >= 0; --i) printf("%c", x_str[i]);
+}
 
-    if (fraction > 0.0)
+void print_fraction(double x, int base)
+{
+    if (x > 0.0)
     {
         printf(".");
         for (int i = 0; i < 12; ++i)
         {
             double digit_double;
-            fraction = (modf(fraction * b2, &digit_double));
+            x = (modf(x * base, &digit_double));
+
             int digit = (int)digit_double;
-            if (digit <= 9)
-                printf("%d", digit);
-            else
-                printf("%c", 'A' + digit - 10);
+
+            if (digit <= 9) printf("%d", digit);
+
+            else printf("%c", 'A' + digit - 10); // ('A' + digit - 10) символ цифры digit, если digit > 9
         }
     }
+}
+
+int main() {
+    int b1, b2;
+    scanf("%d %d\n", &b1, &b2);
+    char X[MAX_LEN_NUMBER];
+    scanf("%s", X);
+
+    int len = strlen(X);
+    int power = power_number(X, len);
+
+    if (power == -1 ||
+        b1 < 2 ||
+        b2 < 2 ||
+        b1 > 16 ||
+        b2 > 16)
+    {
+        printf("bad input\n");
+        return 0;
+    }
+
+    double number = 0;
+
+    for (int i = 0; i < len; ++i) {
+        if (islower(X[i])) X[i] = toupper(X[i]);
+
+        if (bad_digit(X[i], b1))
+        {
+            printf("bad input\n");
+            return 0;
+        }
+
+        if (isalpha(X[i])) number += ((X[i] - 'A' + 10) * pow_int(b1, power)); // (X[i] - 'A' + 10) -
+                                                                               // перевод цифры X[i] из строки в число в случае если X[i] это буква
+
+        else if (X[i] != '.') number += ((X[i] - '0') * pow_int(b1, power)); // (X[i] - '0') - перевод цифры X[i] из строки в число
+
+        power -= (X[i] != '.');
+    }
+
+    double fraction = modf(number, &number);
+    long long whole = (long long)number;
+
+    print_whole (whole, b2);
+    print_fraction (fraction, b2);
+
     printf("\n");
     return 0;
 }

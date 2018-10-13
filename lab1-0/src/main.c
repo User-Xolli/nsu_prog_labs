bfb388ef5c5c55a9b85b685e36dd757e5e11474d
User-Xolli <zzzzzzzzz1122@gmail.com>
2018-10-13 15:30:57 +0700

    done

--- lab1-0/src/main.c
+++ lab1-0/src/main.c
@@ -1,61 +1,82 @@
-#include <stdio.h>
+#include<stdio.h>
 #include<string.h>
+
+#define SIZE 16 // SIZE = 2^k
+
+
+struct Ring_Buffer
+{
+    int write_count;
+    int mask; //mask = 2^k - 1
+    char data[SIZE];
+};
+
+void write(struct Ring_Buffer *buff, const char *value, const int *len_value)
+{
+    for (int i = 0; i < *len_value; ++i)
+    {
+        buff->data[buff->write_count & buff->mask] = value[i];
+        ++buff->write_count;
+    }
+}
+
+char get_elem(const struct Ring_Buffer *buff, const int i)
+{
+    return buff->data[i & buff->mask];
+}
+
 
 int main()
 {
     char pattern[16];
-    scanf("%16s", pattern);
-    int len = strlen(pattern);
-    int d [256];
+    scanf("%[^\n]%*c", pattern);
+    int len = (int) strlen(pattern);
+    int p[256]; //table of shifts
     for (int i = 0; i < 256; ++i)
     {
-        d[i] = len;
+        p[i] = len;
     }
-    for (int i = len - 1; i >= 0; --i)
-    {
-            if (d[(int)pattern[i]] > len - i)
-        {
-            d[(int)pattern[i]] = i + 1;
-        }
-    }
+    int *d = p + 128; //table shifts with negative number
 
     for (int i = 0; i + 1 < len; ++i)
     {
-        if (d[(int)pattern[i]] > len - i - 1)
+        if (d[(int) pattern[i]] > len - i - 1)
         {
-            d[(int)pattern[i]] = len - i - 1;
+            d[(int) pattern[i]] = len - i - 1; //constructing table of shifts
         }
     }
 
-    char text [1001];
-    int pos = len - 1;
-    //int shift = 0;
-    //while (scanf("%s", text))
-    //scanf("%s", text);
-    fgets (text, 1000, stdin);
-    scanf ("%[^\n]%*c", text);
+    struct Ring_Buffer buffer;
+    buffer.write_count = 0;
+    buffer.mask = SIZE - 1;
 
+    int pos = len - 1; //position of pattern`s last symbol in text
+
+    char symbols[16]; //part text
+    int read_symb_count = (int) fread(symbols, sizeof(char), (size_t) (pos - buffer.write_count) + 1, stdin);
+    write(&buffer, symbols, &read_symb_count);
+
+    while (read_symb_count != 0 && pos < buffer.write_count)
     {
-        //printf("***\n");
-        int len_text = strlen(text);
-        //printf("len = %d\nlen_text = %d\ntext = %s\n", len, len_text, text);
-        while (pos <= len_text)
-        {
-            int concurrences = 0;
-            while (text[pos - concurrences] == pattern[len - concurrences - 1] && len - concurrences - 1 >= 0)
-            {
-                printf("%d ", pos - concurrences + 1);
-                ++concurrences;
-            }
-            if (concurrences != len)
-            {
-                printf("%d ", pos - concurrences + 1);
-            }
-            pos += d[(int)text[pos]];
-            //printf("pos + len = %d\n", pos + len);
-        }
-        printf("\n");
-    }
-    return 0;
+        read_symb_count = (int) fread(symbols, sizeof(char), (size_t) (pos - buffer.write_count) + 1, stdin);
+        write(&buffer, symbols, &read_symb_count);
 
+        int concurrences = 0;
+
+        while (get_elem(&buffer, pos - concurrences) == pattern[len - concurrences - 1] && len - concurrences - 1 >= 0)
+        {
+            printf("%d ", pos - concurrences + 1);
+            ++concurrences;
+        }
+        if (concurrences != len)
+        {
+            printf("%d ", pos - concurrences + 1);
+        }
+        pos += d[(int) get_elem(&buffer, pos)];
+
+        read_symb_count = (int) fread(symbols, sizeof(char), (size_t) (pos - buffer.write_count) + 1, stdin);
+        write(&buffer, symbols, &read_symb_count);
+    }
+    printf("\n");
+    return 0;
 }

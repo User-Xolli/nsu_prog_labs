dae46300983938583d71b6cd299c3aacb1d8522f
User-Xolli <zzzzzzzzz1122@gmail.com>
2018-10-26 22:56:30 +0700

    done lab3-1

--- lab3-1/src/main.c
+++ lab3-1/src/main.c
@@ -1,5 +1,63 @@
+#include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-    return EXIT_SUCCESS;
+void swap (int* a, int* b);
+void swap (int* a, int* b)
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+void quick_sort (int *array, int left, int right);
+void quick_sort (int *array, int left, int right) // using Hoare partition scheme
+{
+    if (left < right)
+    {
+        int pivot = array[left];
+        int left_cursor = left - 1;
+        int right_cursor = right + 1;
+
+        do
+        {
+            do
+            {
+                ++left_cursor;
+            }
+            while (array[left_cursor] < pivot);
+
+            do
+            {
+                --right_cursor;
+            }
+            while (array[right_cursor] > pivot);
+
+            if (left_cursor < right_cursor)
+            {
+                swap (&array[left_cursor], &array[right_cursor]);
+            }
+        }
+        while (left_cursor < right_cursor);
+
+        quick_sort (array, left, right_cursor);
+        quick_sort (array, right_cursor + 1, right);
+    }
+}
+
+int main(void)
+{
+    int n;
+    scanf ("%d", &n);
+    int* array = malloc (sizeof(int) * (size_t)n);
+    for (int i = 0; i < n; ++i)
+    {
+        scanf ("%d", &array[i]);
+    }
+    quick_sort (array, 0, n - 1);
+    for (int i = 0; i < n; ++i)
+    {
+        printf ("%d ", array[i]);
+    }
+    printf ("\n");
+    return 0;
 }

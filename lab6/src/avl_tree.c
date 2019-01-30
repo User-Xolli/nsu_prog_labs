f7975e329f7482e86adf374167529489c4c5da99
User-Xolli <zzzzzzzzz1122@gmail.com>
2019-01-31 06:58:38 +0700

    done lab6

---
+++ lab6/src/avl_tree.c
@@ -0,0 +1,151 @@
+/**
+ * \file
+ * \brief Файл с реализацией АВЛ-дерева
+ */
+
+#include "avl_tree.h"
+#include <stdlib.h>
+
+/**
+ *  \brief Возвращает максимум из двух чисел
+ *
+ * \param a Первое число
+ * \param b Второе число
+ * \return Наибольшее из двух чисел
+ */
+static int max(int a, int b)
+{
+    return (a > b) ? a : b;
+}
+
+void initialization(struct avl_tree *root, int data)
+{
+    root->data = data;
+    root->left = NULL;
+    root->right = NULL;
+    root->height = 1;
+}
+
+/**
+ * \brief Разница между поддеревьями
+ *
+ * \note
+ * Если поддерево пусто, то его высота равна нулю
+ *
+ * \param tree указатель на дерево в котором необходимо посчитать разницу высот поддеревьев
+ * \return Разницу высот поддеревьев в дереве tree
+ */
+static int diff_subtrees(struct avl_tree *tree)
+{
+    int height_left = (tree->left != NULL) ? tree->left->height : 0;
+    int height_right = (tree->right != NULL) ? tree->right->height : 0;
+    return height_left - height_right;
+}
+
+/**
+ * \brief Высота дерева
+ *
+ * \param tree указатель на дерево, высоту которого необходимо посчитать
+ */
+static void calc_height(struct avl_tree *tree)
+{
+    int height_left = (tree->left != NULL) ? tree->left->height : 0;
+    int height_right = (tree->right != NULL) ? tree->right->height : 0;
+    tree->height = max(height_left, height_right) + 1;
+}
+
+/**
+ * \brief Левый поворот
+ *
+ * \param a вершина, вокруг которой необходимо совершить левый поворот
+ * \return возвращает вершину, которая оказалась на том месте, на котором раньше находилась вершина a
+ */
+static struct avl_tree *turn_left(struct avl_tree *a)
+{
+    struct avl_tree *b = a->right;
+    a->right = b->left;
+    b->left = a;
+    calc_height(a);
+    calc_height(b);
+    return b;
+}
+
+/**
+ * \brief Правый поворот
+ *
+ * \param a вершина, вокруг которой необходимо совершить правый поворот
+ * \return возвращает вершину, которая оказалась на том месте, на котором раньше находилась вершина a
+ */
+static struct avl_tree *turn_right(struct avl_tree *a)
+{
+    struct avl_tree *b = a->left;
+    a->left = b->right;
+    b->right = a;
+    calc_height(a);
+    calc_height(b);
+    return b;
+}
+
+/**
+ * \brief Балансировка дерева
+ *
+ * Если разница между высотами поддеревьев составила 2, то дерево перестроиться. Если всё в порядке и разница высот менше двух, то дерево никак не изменится
+ *
+ * @param tree дерево, которое необходимо сбалансировать
+ * @return Указатель на сбалансированное дерево
+ */
+static struct avl_tree *balance(struct avl_tree *tree)
+{
+    calc_height(tree);
+    if (diff_subtrees(tree) == -2) // left - right = -2
+    {
+        if (diff_subtrees(tree->right) > 0) // tree->right->left->height > tree->right->right->height
+        {
+            tree->right = turn_right(tree->right);
+        }
+        return turn_left(tree);
+    }
+
+    if (diff_subtrees(tree) == 2) // left - right = 2
+    {
+        if (diff_subtrees(tree->left) < 0)  // tree->right->left->height < tree->right->right->height
+        {
+            tree->left = turn_left(tree->left);
+        }
+        return turn_right(tree);
+    }
+
+    return tree;
+}
+
+struct avl_tree *insert(struct avl_tree *root, int value)
+{
+    if (root == NULL)
+    {
+        struct avl_tree *tmp = malloc(sizeof(struct avl_tree));
+        initialization(tmp, value);
+        return tmp;
+    }
+    if (value > root->data)
+    {
+        root->right = insert(root->right, value);
+    }
+    else
+    {
+        root->left = insert(root->left, value);
+    }
+    return balance(root);
+}
+
+void del_tree(struct avl_tree *root)
+{
+    if (root->left != NULL)
+    {
+        del_tree(root->left);
+    }
+    if (root->right != NULL)
+    {
+        del_tree(root->right);
+    }
+    free(root);
+}
--- lab6/src/main.c
+++ lab6/src/main.c
@@ -1,5 +1,47 @@
-#include <stdlib.h>
+/**
+ * \file
+ * \brief Файл с вводом, выводом и построением дерева
+ */
 
-int main(void) {
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "avl_tree.h"
+
+int main(void)
+{
+    int n;
+    scanf("%d", &n);
+    struct avl_tree *tree = malloc(sizeof(struct avl_tree));
+    if (n > 0)
+    {
+        int tmp;
+        scanf("%d", &tmp);
+        initialization(tree, tmp);
+    }
+    else
+    {
+        printf("0\n");
+        free(tree);
+        return EXIT_SUCCESS;
+    }
+
+    for (int i = 0; i + 1 < n; ++i)
+    {
+        int tmp;
+        scanf("%d", &tmp);
+        tree = insert(tree, tmp);
+    }
+    printf("%d\n", tree->height);
+    del_tree(tree);
     return EXIT_SUCCESS;
 }
+
+/**
+ * \mainpage Лабораторная работа №6, НГУ ФИТ 1 курс
+ * \author Xolli
+ * \date 31.01.2019
+ * \version 1.0
+ *
+ * Моя вторая в жизни документация)
+ */
